a70175167e4c8a66c3f1719cd8771850735f7bba
aravind <ampadiaravindam@gmail.com>
2020-09-23 10:50:36 +0530

    first

--- app/src/main/java/com/example/reubro_json2/MainActivity.java
+++ app/src/main/java/com/example/reubro_json2/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.reubro_json2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    MyAdapter myAdapter;
+    String s1,s2;
+    RecyclerView recyclerView;
+    String JSON_STRING = "{\"JSON\":{\"status\":\"success\",\"message\":\"Login Success\",\"data\":{\"user_id\":3544354,\"title\":0,\"user_name\":\"Gilson Gilbert\",\"email_id\":\"gilson @gmail.com\",\"dob\":\"15-12-1990\",\"phone\":9845268745,\"country\":\"+91\",\"profile_pic\":\"https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.shutterstock.com%2Fimage-photo%2Fwhite-transparent-leaf-on-mirror-260nw-577160911.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fimage&tbnid=saXt3gObqm30jM&vet=1&docid=QooZsUX3wOLfLM&w=382&h=280&source=sh%2Fx%2Fim\"}}}";
+    String jstatus,jmessage,juserid,jtitle,jname, jmail,jdob,jphone,jcontry,jimag;
+
+    String jsonstring[];
+    TextView employeeName, employeeSalary;
+    ImageView imageview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        try {
+
+            JSONObject obj = new JSONObject(JSON_STRING);
+
+            JSONObject jmain = obj.getJSONObject("JSON");
+            JSONObject jsub  = jmain.getJSONObject("data");
+
+            jstatus=jmain.getString("status");
+            jmessage=jmain.getString("message");
+            juserid = jsub.getString("user_id");
+            jtitle= jsub.getString("title");
+            jname = jsub.getString("user_name");
+            jmail= jsub.getString("email_id");
+            jdob= jsub.getString("dob");
+            jphone= jsub.getString("phone");
+            jcontry= jsub.getString("country");
+            jimag= jsub.getString("profile_pic");
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        recyclerView=findViewById(R.id.recycler);
+
+
+
+
+        MyAdapter myAdapter =new MyAdapter(this,jstatus,jmessage,juserid,jtitle,jname, jmail,jdob,jphone,jcontry,jimag);
+
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
---
+++ app/src/main/java/com/example/reubro_json2/MyAdapter.java
@@ -0,0 +1,95 @@
+package com.example.reubro_json2;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+
+    String      status,message,userid,title,name, mail,dob,phone,contry,imag;
+
+    Context context;
+
+    public MyAdapter(Context ct,String jstatus,String jmessage,String juserid,String jtitle,String jname,String  jmail,String jdob,String jphone,String jcontry,String jimg){
+
+
+        context=ct;
+        status=jstatus;
+        message=jmessage;
+        userid=juserid;
+        title=jtitle;
+        name=jname;
+        mail=jmail;
+        dob=jdob;
+        phone=jphone;
+        contry=jcontry;
+        imag=jimg;
+
+        // images=image;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent , false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int i) {
+        holder.mytext1.setText(status);
+        holder.mytext2.setText(message);
+        holder.mytext3.setText(userid);
+        holder.mytext4.setText(title);
+        holder.mytext5.setText(name);
+        holder.mytext6.setText(mail);
+        holder.mytext7.setText(dob);
+        holder.mytext8.setText(phone);
+        holder.mytext9.setText(contry);
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mytext1,mytext2,mytext3,mytext4,mytext5,mytext6,mytext7,mytext8,mytext9;
+        ImageView myimage;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mytext1=itemView.findViewById(R.id.textView);
+            mytext2=itemView.findViewById(R.id.textView2);
+            mytext3=itemView.findViewById(R.id.textView3);
+            mytext4=itemView.findViewById(R.id.textView4);
+            mytext5=itemView.findViewById(R.id.textView5);
+            mytext6=itemView.findViewById(R.id.textView6);
+            mytext7=itemView.findViewById(R.id.textView7);
+            mytext8=itemView.findViewById(R.id.textView8);
+            mytext9=itemView.findViewById(R.id.textView9);
+            myimage=itemView.findViewById(R.id.imageViewx);
+        }
+    }
+}
